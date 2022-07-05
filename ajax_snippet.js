27afc3e5be744005de711b5e8648e91c2e1b1e1d
Mahammmadhusain Kadiwala <62128991+Mahammadhusain@users.noreply.github.com>
2022-07-05 11:18:34 +0530

    Update ajax_snippet.js

--- ajax_snippet.js
+++ ajax_snippet.js
@@ -1,7 +1,12 @@
 
 <script>
     function add_to_cart(prod_id) {
-    let crs = document.getElementsByName('csrfmiddlewaretoken')[0].value
+    let crs = document.getElementsByName('csrfmiddlewaretoken')
+      console.log(crs);
+      my_data = {
+        prod_id:prod_id,
+        csrfmiddlewaretoken:crs,
+      }
     // put your logics here      
 
       $.ajax({
