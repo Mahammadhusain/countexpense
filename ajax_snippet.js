f2785bfe2053216df7d6c82c81c8f4849c11930e
Mahammmadhusain Kadiwala <62128991+Mahammadhusain@users.noreply.github.com>
2022-07-05 11:02:23 +0530

    Update ajax_snippet.js

--- ajax_snippet.js
+++ ajax_snippet.js
@@ -1,1 +1,17 @@
 
+<script>
+    function add_to_cart(prod_id) {
+
+    // put your logics here      
+
+      $.ajax({
+      url: "/creditsave/",
+      method: "POST",
+      data: my_data,
+      success: function (data){
+        
+      }
+    });
+
+    }
+  </script>
